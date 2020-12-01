2073affb2888f2c0eb342155580f74e78bfed869
Stephen Young <stephen@customer.io>
2020-12-01 15:39:46 -0500

    part II: inefficient, but it works!

--- 2020/dec1/sum2020_multiply.adb
+++ 2020/dec1/sum2020_multiply.adb
@@ -6,37 +6,38 @@ with Ada.Integer_Text_IO;           use Ada.Integer_Text_IO;
 
 procedure Sum2020_Multiply is
 
- -- Since our value will sum to 2020, we assume the values in the
-   -- file will be less than 2020
-   type SumNum is range 0 .. 2020;
-
    -- Not going to bother figuring out how to do this dynamically  
    type Index is range 0 .. 200;
-   Numbers : array (Index) of SumNum;
+   Numbers : array (Index) of Long_Integer;
 
    F : File_Type;
    I : Index := 0;
 
-   Result : Integer := 0;
+   Result : Long_Integer := 0;
 
 begin
    Ada.Text_IO.Put_Line ("Hello, December First"); 
 
    Open (F, Mode => In_File, Name => "input.txt");
    While not  End_Of_File (F) loop
-      Numbers(I) := SumNum(Integer'Value (Get_Line (F)));
+      Numbers(I) := Long_Integer(Integer'Value (Get_Line (F)));
       I := I + 1;
    end loop;
    Close (F);
 
    For J in Numbers'Range loop
       For K in Numbers'Range loop
-         if Numbers(J)+Numbers(K) = 2020 then
-            Result := Integer(Numbers(J)*Numbers(K));
-            exit;
+         if Numbers(J)+Numbers(K) < 2020 then
+            For L in Numbers'Range loop
+               if Numbers(J)+Numbers(K)+Numbers(L) = 2020 then
+                  Result := Numbers(J)*Numbers(K) * Numbers(L);
+                  exit;
+               end if;
+            end loop;
          end if;
          exit when Result > 0;
       end loop;
+      exit when Result > 0;
    end loop;
 
    Put_Line("Result:" & Integer'Image(Integer(Result)));
