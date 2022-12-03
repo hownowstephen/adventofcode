771b67a3c274ab83d7c23068897f0f9169ac6c9c
Stephen Young <stephen@customer.io>
2022-12-03 18:56:16 -0500

    2022 dec3 part 22022 dec3 part 2

--- 2022/dec3/main.go
+++ 2022/dec3/main.go
@@ -20,33 +20,37 @@ func main() {
 
 	s.Split(bufio.ScanLines)
 
-	var b = [2]*fixed.Bitmap{
+	var b = [3]*fixed.Bitmap{
+		fixed.NewBitmap(122 - 65),
 		fixed.NewBitmap(122 - 65),
 		fixed.NewBitmap(122 - 65),
 	}
 
 	var total uint32
+	var idx int
 	for s.Scan() {
 		line := s.Text()
 		for i := range line {
-			b[i/(len(line)/2)].AddInt(int(line[i]) - 65)
+			b[idx%3].AddInt(int(line[i]) - 65)
 		}
 
-		b[0].And(b[1])
+		if idx%3 == 2 {
+			b[0].And(b[1])
+			b[0].And(b[2])
 
-		// 65 = A = 27
-		// 66 = B = 28
-
-		for _, v := range b[0].ToArray() {
-			if v > 32 {
-				total += v - 31
-			} else {
-				total += v + 27
+			for _, v := range b[0].ToArray() {
+				if v > 32 {
+					total += v - 31
+				} else {
+					total += v + 27
+				}
 			}
-		}
 
-		b[0].Clear()
-		b[1].Clear()
+			b[0].Clear()
+			b[1].Clear()
+			b[2].Clear()
+		}
+		idx++
 	}
 
 	fmt.Println(total)
