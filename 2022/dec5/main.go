f1a2f22fd1d2d9c4753a7683be9a7febca5a1954
Stephen Young <stephen@customer.io>
2022-12-06 22:36:34 -0500

    2022 dec 5th part one

---
+++ 2022/dec5/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+
+	f, err := os.Open("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	s := bufio.NewScanner(f)
+
+	s.Split(bufio.ScanLines)
+
+	var buckets [][]byte
+	for s.Scan() {
+
+		t := s.Text()
+		if strings.TrimSpace(t) == "" {
+			break
+		}
+
+		var b []byte
+		for i := 0; i < len(t); i += 4 {
+			b = append(b, t[i+1])
+		}
+
+		buckets = append(buckets, b)
+	}
+
+	// bucket ids, not needed
+	buckets = buckets[:len(buckets)-1]
+
+	stacks := make([][]byte, len(buckets[0]))
+	for i := len(buckets) - 1; i >= 0; i-- {
+		for j, e := range buckets[i] {
+			if e == byte(' ') {
+				continue
+			}
+			stacks[j] = append(stacks[j], e)
+		}
+	}
+
+	for s.Scan() {
+		command := strings.Split(s.Text(), " ")
+
+		count, src, dst := fi(command[1]), fi(command[3]), fi(command[5])
+
+		for i := 0; i < count; i++ {
+			stacks[dst-1] = append(stacks[dst-1], stacks[src-1][len(stacks[src-1])-1])
+			stacks[src-1] = stacks[src-1][:len(stacks[src-1])-1]
+		}
+	}
+
+	for _, stack := range stacks {
+		fmt.Print(string(stack[len(stack)-1]))
+	}
+	fmt.Println()
+
+}
+
+func fi(s string) int {
+	i, _ := strconv.Atoi(s)
+	return i
+}
